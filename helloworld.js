0aace0c4cb76e265fdb7b1fb837fe1e8f5cecf48
vandosant <sjskender@gmail.com>
2016-03-22 12:45:04 -0600

    Immediate error handling in module

--- helloworld.js
+++ helloworld.js
@@ -1,5 +1,13 @@
 function say(filename, cb) {
-  return fs.readFile(filename, cb);
+  return fs.readFile(filename, function(err, contents) {
+    if (err) {
+      cb(err);
+    } else {
+      setTimeout(function() {
+        cb(null, contents);
+      },1000);
+    }
+  });
 }
 
 var fs = require("fs");
