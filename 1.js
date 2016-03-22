b1db41e6b7b61d2044fde00c4717a5d95e28dd45
vandosant <sjskender@gmail.com>
2016-03-22 12:05:14 -0600

    Add help functionality

--- 1.js
+++ 1.js
@@ -2,4 +2,17 @@ var args = require("minimist")(process.argv.slice(2), { string: "name"} );
 
 var name = args.name;
 
+function printHelp() {
+  console.log("1.js (c) Scott Skender");
+  console.log("");
+  console.log("usage:");
+  console.log("--help	print this help");
+  console.log("--name 	say hello to {NAME}");
+  console.log("");
+}
+
+if (!name || args.help) {
+  printHelp();
+  process.exit();
+}
 console.log("Hello " + name);
