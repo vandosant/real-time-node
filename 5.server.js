71748af7fe8dedc7c99a389f6361d7b272f103cc
vandosant <sjskender@gmail.com>
2016-04-17 17:55:08 -0600

    Promise style async

--- 5.server.js
+++ 5.server.js
@@ -1,5 +1,5 @@
 var http = require('http');
-
+var ASQ = require('asynquence');
 var host = 'localhost';
 var port = 8000;
 var httpServer = http.createServer(handleHTTP).listen(port, host);
@@ -7,16 +7,19 @@ function handleHTTP(req, res) {
   if (req.method === "GET") {
     if (req.url === "/") {
       res.writeHead(200, {"Content-type": "text/plain"});
-      var rand = 0;
-      var hello = null;
-      setTimeout(function() { 
-        rand = Math.random();
-        setTimeout(function() {
-          hello = "Hello World " + rand;
-          res.write(hello);
-          res.end();
-        }, 500);
-       }, 500);
+      ASQ()
+      .then(function(done) {
+        setTimeout(function () {
+          done(Math.random());
+        }, 500)
+      }).then(function(done, rand) {
+        setTimeout(function () {
+          done("Hello World: " + rand);
+        }, 500)
+      }).then(function(done, str) {
+        res.write(str)
+        res.end();
+      })
     } else {
       res.writeHead(403);
       res.end();
