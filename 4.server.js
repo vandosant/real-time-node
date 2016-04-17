d0e7c0c309d526b4302de8dbfe48c82b49c9f14c
vandosant <sjskender@gmail.com>
2016-04-17 17:42:21 -0600

    Add root routing to server.js

--- 4.server.js
+++ 4.server.js
@@ -4,5 +4,17 @@ var host = 'localhost';
 var port = 8000;
 var httpServer = http.createServer(handleHTTP).listen(port, host);
 function handleHTTP(req, res) {
-  res.end("Hello World");
+  if (req.method === "GET") {
+    if (req.url === "/") {
+      res.writeHead(200, {"Content-type": "text/plain"});
+      res.write("Hello World");
+      res.end(Math.random().toString());
+    } else {
+      res.writeHead(403);
+      res.end();
+    }
+  } else {
+    res.writeHead(403);
+    res.end();
+  }
 }
